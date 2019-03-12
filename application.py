a7bdc61031c3d55c7bfb9809ba80a4700e723f4c
evie9z <zyw999999999@gmail.com>
2019-03-12 00:33:12 -0700

    Fourth full backend

--- application.py
+++ application.py
@@ -273,5 +273,5 @@ if __name__ == "__main__":
     print("Start running!")
     print()
     print("I'm so excited!!!!")
-    app.run()
+    app.run(host="localhost", port=5000, debug=True)
     # app.run(host='https://test-n-one-smart-kitchen.azurewebsites.net', port=8000, debug=True)
